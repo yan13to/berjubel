93278f55d571dc3373e11345e65357810b62a8a3
Yanto Daryanto <adnorty@gmail.com>
2019-12-03 01:39:24 +0700

    add react-rails

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'devise'
 gem 'exception_notification'
+gem 'money'
+gem 'react-rails'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -25,3 +25,7 @@ $(function() {
 });
                 
 
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
