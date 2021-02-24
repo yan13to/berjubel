ba8af8be9c027eeb1360f239f49e4fb8e04eb9ab
Yanto Daryanto <yan13to@gmail.com>
2021-02-24 21:55:03 +0700

    some ui changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@
 # ApplicationHelper
 module ApplicationHelper
   def render_error_message(type, message)
-    render 'shared/error_message', type: type, message: message if message.present?
+    render 'error_message', type: type, message: message if message.compact.present?
+  end
+
+  def render_error_message_for(messages)
+    render 'error_message_for', messages: messages if messages.compact.present?
   end
 end
