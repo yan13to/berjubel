b67d8c005c1544ba3f7b6f9a07f6b305cdd5f243
Yanto Daryanto <yan13to@gmail.com>
2021-06-09 10:00:27 +0700

    fix typo

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -4,9 +4,9 @@ module Api
   module V1
     # ApplicationController
     class ApplicationController < ActionController::API
-      before_action :authenticate_api_v1_user!
+      # before_action :authenticate_api_v1_user!
 
-      alias current_user current_api_v1_user
+      # alias current_user current_api_v1_user
     end
   end
 end
